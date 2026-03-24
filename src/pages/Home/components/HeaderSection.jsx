993e314ed379b7d181fc11cfb3d6940e7c9eb100
Sunday Alabi <iamalabisunday@gmail.com>
2026-03-24 23:52:54 +0100

    Navbar Section

--- src/pages/Home/components/HeaderSection.jsx
+++ src/pages/Home/components/HeaderSection.jsx
@@ -24,7 +24,7 @@ function HeaderLink({ to, text }) {
 export default function HeaderSection() {
     return (
         <section className="py-16 sm:py-24 lg:py-6 bg-primaryLighter">
-            <div className="mx-auto max-w-7xl px-4 pb-6 sm:px-6 lg:px-8 border-b-[0.05rem] border-b-primary">
+            <div className="mx-auto max-w-7xl pb-6 sm:px-6 md:px-0 border-b-[0.05rem] border-b-primary">
                 <div className="flex items-center justify-between">
                     <img src={Logo} alt="Logo" className="w-32" />
                     <div className="flex items-center gap-8 font-light text-sm text-textBody">
